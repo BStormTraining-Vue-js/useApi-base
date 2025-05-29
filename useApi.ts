775dac386c4a401cf0f57df21d20f62d8246cadd
khunly82 <92391344+khunly82@users.noreply.github.com>
2025-05-29 23:35:04 +0200

    Update useApi.ts

--- useApi.ts
+++ useApi.ts
@@ -1,9 +1,9 @@
 import { ref, type Ref } from 'vue';
 
 export namespace useApi {
-    export type ApiResult<T> = {data: Ref<T|undefined>, error: Ref<any>, isLoading: Ref<boolean>};
-    export type ApiCallBack = (...args: any[]) => void;
-    export type ApiRequest<T> = (...args: any[]) => Promise<T>;
+    export type ApiResult<T> = [data: Ref<T|undefined>, error: Ref<unknown>, isLoading: Ref<boolean>];
+    export type ApiCallBack = (...args: unknown[]) => void;
+    export type ApiRequest<T> = (...args: unknown[]) => Promise<T>;
 }
 
 export function useApi<T>(apiRequest: useApi.ApiRequest<T>): [useApi.ApiCallBack, useApi.ApiResult<T>] {
@@ -11,12 +11,12 @@ export function useApi<T>(apiRequest: useApi.ApiRequest<T>): [useApi.ApiCallBack
     const error = ref<any>();
     const isLoading = ref<boolean>(false);
 
-    async function callback(...args: any[]) {
+    async function callback(...args: unknown[]) {
         isLoading.value = true;
         try {
             const response = await apiRequest(...args);
             data.value = response
-        } catch(err: any) {
+        } catch(err) {
             error.value = err
         } finally {
             isLoading.value = false
